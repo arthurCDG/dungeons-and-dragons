95f7ee7d4d920eb4e25c03707a83d951e3470d2a
achastenet <achastenetdegery@lucca.fr>
2024-05-04 09:43:11 +0200

    UPD EF

--- back/Infra/Migrations/GlobalDbContextModelSnapshot.cs
+++ back/Infra/Migrations/GlobalDbContextModelSnapshot.cs
@@ -17,7 +17,7 @@ namespace dnd_infra.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "8.0.2")
+                .HasAnnotation("ProductVersion", "8.0.4")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128);
 
             SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder);
