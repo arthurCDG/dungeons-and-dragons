1d239af632b471dda4dfcbf33dba39bf09d9920d
achastenet <achastenetdegery@lucca.fr>
2024-06-12 00:10:41 +0200

    ADD availability for DM

--- front/src/app/pages/players/players-page/players-page.component.ts
+++ front/src/app/pages/players/players-page/players-page.component.ts
@@ -2,42 +2,62 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, RouterModule } from '@angular/router';
 
-import { IPlayer, PlayerRole } from '../../../models';
-import { PlayersService } from '../../../services';
-import { BackArrowComponent, PlayerCardComponent, SelectedPlayerComponent, PageWrapperComponent, PageBackgroundImageComponent, ImageType, LoadingSpinnerComponent } from '../../../components';
+import { firstValueFrom } from 'rxjs';
+import {
+	BackArrowComponent,
+	ImageType,
+	LoadingSpinnerComponent,
+	PageBackgroundImageComponent,
+	PageWrapperComponent,
+	PlayerCardComponent,
+	SelectedPlayerComponent
+} from '../../../components';
+import { IPlayer, IUserDto, PlayerRole } from '../../../models';
+import { AvailableDungeonMastersService, PlayersService, UsersService } from '../../../services';
 
 @Component({
-  selector: 'app-players-page',
-  standalone: true,
-  imports: [
-	CommonModule,
-	RouterModule,
-	PlayerCardComponent,
-	BackArrowComponent,
-	SelectedPlayerComponent,
-	PageWrapperComponent,
-	PageBackgroundImageComponent,
-	LoadingSpinnerComponent
-],
-  templateUrl: './players-page.component.html',
-  styleUrls: ['./players-page.component.css'],
-  providers: [PlayersService]
+	selector: 'app-players-page',
+	standalone: true,
+	imports: [
+		CommonModule,
+		RouterModule,
+		PlayerCardComponent,
+		BackArrowComponent,
+		SelectedPlayerComponent,
+		PageWrapperComponent,
+		PageBackgroundImageComponent,
+		LoadingSpinnerComponent
+	],
+	templateUrl: './players-page.component.html',
+	styleUrls: ['./players-page.component.css'],
+	providers: [
+		PlayersService,
+		UsersService,
+		AvailableDungeonMastersService
+	]
 })
 export class PlayersPageComponent implements OnInit {
-	public players: IPlayer[] = [];
+	public players: IPlayer[]
 	public currentPlayer: IPlayer;
 	public isLoading = true;
-	public isDungeonMaster = false;
+
+	public user: IUserDto;
 
 	public readonly maxPlayersCount = 4;
 	public backgroundImage: ImageType = 'players-creation-page';
 
 	private userId: number;
 
-	constructor(private readonly playersService: PlayersService, private readonly route: ActivatedRoute) { }
+	constructor(
+		private readonly playersService: PlayersService,
+		private readonly usersService: UsersService,
+		private readonly availableDungeonMastersService: AvailableDungeonMastersService,
+		private readonly route: ActivatedRoute
+	) { }
 
-	ngOnInit(): void {
-		this.route.params.subscribe(params => this.userId = Number(params['userId']));
+	async ngOnInit(): Promise<void> {
+		this.userId = await firstValueFrom(this.route.params).then(params => Number(params['userId']));
+
 		this.playersService
 			.getAsync(this.userId)
 			.subscribe((players: IPlayer[]) => {
@@ -45,12 +65,27 @@ export class PlayersPageComponent implements OnInit {
 				if (players.length > 0) {
 					this.currentPlayer = players[0];
 				}
-				if (players.some(p => p.profile?.role === PlayerRole.Monster)) {
-					console.log('passing here?')
-					this.isDungeonMaster = true; // TODO retrieve DM role another way
-				}
-				this.isLoading = false;
-			});
+			})
+
+		this.usersService
+			.getByIdAsync(this.userId)
+			.subscribe((user: IUserDto) => {
+				this.user = user;
+				});
+				
+		this.isLoading = false;
+	}
+
+	public toggleUserAvailability(): void {
+		if (this.user.isAvailable) {
+			this.availableDungeonMastersService
+				.markAsUnavailableAsync(this.userId)
+				.subscribe(() => this.user.isAvailable = false);
+		} else {
+			this.availableDungeonMastersService
+				.markAsAvailableAsync(this.userId)
+				.subscribe(() => this.user.isAvailable = true);
+		}
 	}
 
 	public isSelected(player: IPlayer): boolean {
