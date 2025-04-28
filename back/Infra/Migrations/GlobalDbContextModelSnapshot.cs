3fe2196af5f6912a9927affba33ff251c8018df8
arthurCDG <arthur.chastenet@gmail.com>
2025-04-28 11:29:52 +0200

    chore(back): bump nugets

--- back/Infra/Migrations/GlobalDbContextModelSnapshot.cs
+++ back/Infra/Migrations/GlobalDbContextModelSnapshot.cs
@@ -17,7 +17,7 @@ namespace dnd_infra.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "8.0.6")
+                .HasAnnotation("ProductVersion", "9.0.4")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128);
 
             SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder);
